b719029d96280da465c533d7731fdd17138c5d13
Alejo Flores Lucey <alejo@misterflores.com>
2022-09-25 21:28:51 +0000

    Finished heap memory manager

--- Kernel/heap_manager.c
+++ Kernel/heap_manager.c
@@ -4,19 +4,19 @@ uint64_t total_memory;
 uint64_t used_memory;
 uint64_t memory_node_count;
 
-typedef struct{
+typedef struct memory_node_cdt {
     uint64_t size;
     uint64_t leftover;
-    (List_CDT *) next;
-    (List_CDT *) previous;
-}Memory_Node_CDT;
+    struct memory_node_cdt * next;
+    struct memory_node_cdt * previous;
+} Memory_Node_CDT;
 
 typedef Memory_Node_CDT * Memory_Node_ADT;
 
 Memory_Node_ADT head = NULL;
 
 //Crea la lista de memoria, recibe la direcc de memoria donde arranca el heap y el size del mismo.
-void memory_manager_start( (void *) start_address, uint64_t size_heap){
+void memory_manager_start(void * start_address, uint64_t size_heap){
     total_memory = size_heap;
     used_memory = sizeof( Memory_Node_CDT );
     memory_node_count = 1;
@@ -28,7 +28,7 @@ void memory_manager_start( (void *) start_address, uint64_t size_heap){
 }
 
 //Toma la cantidad necesaria de nodos de la lista para alocar el size requested.
-void * memory_manager_alloc( uint64_t size ){
+void * memory_manager_alloc(uint64_t size){
     if(head == NULL || size == 0 || total_memory - used_memory < size){
         return NULL;
     }
@@ -43,7 +43,7 @@ void * memory_manager_alloc( uint64_t size ){
         current->size = size;
         current->leftover -= size;
         used_memory += size;
-        return ( void * ) current + sizeof(Memory_Node_CDT);
+        return (void *) current + sizeof(Memory_Node_CDT);
     }else{
         Memory_Node_ADT new_node = current + current->size + sizeof(Memory_Node_CDT);
         new_node->size = size;
@@ -57,17 +57,37 @@ void * memory_manager_alloc( uint64_t size ){
         current->next = new_node;
         used_memory += size + sizeof(Memory_Node_CDT);
         memory_node_count++;
-        return ( void * ) new_node + sizeof(Memory_Node_CDT);
+        return (void *) new_node + sizeof(Memory_Node_CDT);
     }
 }
 
 /*Libera la memoria y agrega un/unos nodo/s a la lista de memoria libre. Que si tienen 
 nodos contiguos libres los junta.*/
-void memory_manager_free( (void *)ptr ){
-    
+void memory_manager_free(void * ptr){
+    if (ptr == NULL) {
+        return;
+    }
+    Memory_Node_ADT current = ptr - sizeof(Memory_Node_CDT);
+    if (current->previous == NULL) {
+        current->leftover += current->size;
+        used_memory -= current->size;
+        current->size = 0;
+    } else {
+        Memory_Node_ADT prev = current->previous;
+        prev->next = current->next;
+        if (current->next != NULL) {
+            current->next->previous = prev;
+        }
+        prev->leftover += current->size + current->leftover + sizeof(Memory_Node_CDT);
+        used_memory -= current->size + sizeof(Memory_Node_CDT);
+        memory_node_count--;
+    }
 }
 
 //Devuelve a traves de Memory_State informacion sobre la memoria.
-void memory_manager_get_state((Memory_State *) memory_state){
-
+void memory_manager_get_state(Memory_State * memory_state){
+    memory_state->memory_total = total_memory;
+    memory_state->memory_occupied = used_memory;
+    memory_state->memory_free = total_memory - used_memory;
+    memory_state->memory_nodes = memory_node_count;
 }
