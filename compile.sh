4ff6aadeb4bc125a7f1a5c08fd20ad0491aa3cd5
Alejo Flores Lucey <alejo@misterflores.com>
2022-10-06 14:25:17 +0000

    Removed .elf and unnecesary files

--- compile.sh
+++
@@ -1,9 +0,0 @@
-docker start arqui
-docker exec -it arqui make clean -C /root/Toolchain
-docker exec -it arqui make clean -C /root/
-if [[ "$1" != "clean" ]]
-then
-    docker exec -it arqui make -C /root/Toolchain
-    docker exec -it arqui make -C /root/
-fi
-docker stop arqui
